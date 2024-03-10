e1e29b232bc142df32b80b2ebf39414e43d9e01a
BurhanArslann <arslann.burhan.git@gmail.com>
2024-03-10 18:05:47 +0300

    ornek kodlar eklendi

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,6 +1,23 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
+  
+  const [sayac, sayacGuncelle] = useState(0)
+
+  useEffect(() => {
+    console.log("1. effect calısti");
+  },[])
+  useEffect(() => {
+    console.log("2. effect calısti");
+  })
+  
+
+
+  return (
+    <>
+      sayaç:{sayac}
+    </>
+  )
 
 }
 
