b52a8b2500c2ce5978696bf03ffdacfc4effa9ae
Burhan Arslan <arslann.burhan.git@gmail.com>
2024-03-11 12:52:28 +0300

    Girilen 2 sayi aralığında random sayi uretilme fonksiyonu tamamlandı

--- project1/src/App.js
+++ project1/src/App.js
@@ -6,7 +6,41 @@ import "./index.css"
 function App
 () {
     
- 
+    const [min, setMin] = useState()
+    const [max, setMax] = useState()
+    const [random, setRandom] = useState()
+
+    const changeMax =(e) =>{
+        setMax(Number(e.target.value))
+    }
+    
+    const changeMin = (e) =>{
+        setMin(Number(e.target.value))
+    }
+
+    const generateRandom = ()=>{
+      
+        setRandom(Math.floor(Math.random()*(max-min +1)))
+    }
+
+    return(
+        <div className="App">
+            <h1>Ranom Number</h1>
+            <div className="container">
+                <p>Random Number:{random}</p>
+                <input className="form-control" aria-label="Dollar amount"
+                 placeholder="Enter a min number"
+                 onChange={changeMin}
+                />
+                <input type="number"
+                 placeholder="Enter a max number" 
+                 onChange={changeMax}
+                />
+                <button onClick={generateRandom}>Give a Random Number</button>
+                
+            </div>
+        </div>
+    )
 
 }
 
